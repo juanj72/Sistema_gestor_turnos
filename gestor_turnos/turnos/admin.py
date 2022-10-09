964c320e18fc8f49d3b559c8361a551b8f61619d
juanj72 <juan.jara@campusucc.edu.co>
2022-10-09 12:26:08 -0500

    agregacion modelos en admin

--- gestor_turnos/turnos/admin.py
+++ gestor_turnos/turnos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Personal)
+admin.site.register(Caja)
+admin.site.register(Turno)
