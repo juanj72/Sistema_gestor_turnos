f83f87680e2799ee6c8bf3a6b6730a5a84416d40
juanj72 <juan.jara@campusucc.edu.co>
2022-10-11 22:20:13 -0500

    prueba azure

--- turnos/urls.py
+++ turnos/urls.py
@@ -5,8 +5,8 @@ from .import views
 
 urlpatterns = [
     path('',views.inicio,name="inicio"),
-    path('/registro',views.registro,name="registro"),
-    path('/turno',views.turno,name='turno'),
-    path('/tetris',views.tetris,name="tetris"),
+    path('registro',views.registro,name="registro"),
+    path('turno',views.turno,name='turno'),
+    path('tetris',views.tetris,name="tetris"),
     path('log',views.sesion,name="log")
 ]
