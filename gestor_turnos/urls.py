3970325817f3c34fb7857068cc33195566639187
juanj72 <juan.jara@campusucc.edu.co>
2022-10-11 22:29:07 -0500

    agregacion ruta raiz

--- gestor_turnos/urls.py
+++ gestor_turnos/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
  
 from django.contrib import admin
 from django.urls import path, include
+from turnos import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("turnos.urls"))
+    path('',include("turnos.urls")),
+    path('/tetris',views.tetris,name="tetris"),
    
     
 ]
--- turnos/urls.py
+++ turnos/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('',views.inicio,name="inicio"),
     path('registro',views.registro,name="registro"),
     path('turno',views.turno,name='turno'),
-    path('tetris',views.tetris,name="tetris"),
+    
     path('log',views.sesion,name="log")
 ]
